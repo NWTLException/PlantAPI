bcf947690f9aad50a7a0b08fc01a85f785b63c7d
ElChapo-Programming <grakid18@htl-kaindorf.at>
2022-02-21 00:21:23 +0100

    drfgh

--- static/request.js
+++ static/request.js
@@ -12,3 +12,28 @@ function getAllUsers(){
         document.getElementById("user").innerHTML=html;
     })
 }
+
+var i = 1;                  //  set your counter to 1
+
+function loopRequest() {         //  create a loop function
+  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+       //  your code here
+        getHumidityAndTemperature();               //  increment the counter
+    if (i < 2) {           //  if the counter < 10, call the loop function
+      loopRequest();             //  ..  again which will trigger another 
+    }                       //  ..  setTimeout()
+  }, 3000)
+}
+
+    
+
+
+function getHumidityAndTemperature(){
+
+    fetch('http://127.0.0.1:8000/temphum')
+    .then(response=>response.json())
+    .then(data=>{
+        document.getElementById("tftemperature").innerHTML=data.element.temp;
+        document.getElementById("tfhumidity").innerHTML=data.element.humidity;
+    })
+}
